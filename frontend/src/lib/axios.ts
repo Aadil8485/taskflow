a6403fdaa4250706b2d56bd2499d004e7fd0668e
unknown <aadilsyed375@gmail.com>
2026-09-11 23:21:35 +0530

    update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://taskflow-backend-4ika.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
